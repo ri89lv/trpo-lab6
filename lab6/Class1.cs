1fd56b16388b4b221df407e8c33e75871c2260a5
lv <jkhbjk>
2019-05-16 14:26:34 +0700

    titele test added

--- lab6/Class1.cs
+++ lab6/Class1.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace lab6
 {
     public class Class1
     {
+        IWebDriver webDriver = new ChromeDriver();
+
+        [TestCase]
+        public void title()
+        {
+            webDriver.Url = "https://www.youtube.com";
+            Assert.AreEqual("YouTube", webDriver.Title);
+            //webDriver.Close();
+        }
+        [TearDown]
+        public void testend()
+        {
+            //webDriver.Quit();
+        }
     }
 }
