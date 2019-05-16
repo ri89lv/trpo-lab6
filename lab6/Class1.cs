7e844b3d6ac4913bd17a27eef509991cc11a2aa2
lv <jkhbjk>
2019-05-16 14:55:29 +0700

    string added

--- lab6/Class1.cs
+++ lab6/Class1.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace lab6
 {
@@ -20,6 +21,43 @@ namespace lab6
             Assert.AreEqual("YouTube", webDriver.Title);
             //webDriver.Close();
         }
+
+        [TestCase]
+        public void string_test()
+        {
+            webDriver.Url = "https://www.youtube.com";
+            IWebElement search = webDriver.FindElement(By.XPath("//*[@id=\"search\"]"));
+            search.SendKeys("BTS-Boy with luv");
+
+            var wait = new WebDriverWait(webDriver, new TimeSpan(0, 0, 10));
+            //установка условия окончания ожидания
+            var element = wait.Until(condition =>
+            {
+                try
+                {
+                    //до тех пор, пока указанный элемент не станет видим (Displayed == true)
+                    var elementToBeDisplayed =
+                    webDriver.FindElement(By.XPath("//*[@id=\"search-icon-legacy\"]"));
+                    return elementToBeDisplayed.Displayed;
+                }
+                //в случае, если такого элемента нет
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+            //если элемент стал видим, получение ссылки и эмуляция клика //*[@id="search-icon-legacy"]
+            IWebElement button =
+             webDriver.FindElement(By.XPath("//*[@id=\"search-icon-legacy\"]"));
+            button.Click();
+
+        
+        }
+
         [TearDown]
         public void testend()
         {
